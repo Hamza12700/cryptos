b1bfae59c6cd33feaeda597357ce810f5891e9d9
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-05 19:30:39 +0400

    refactor: created `hasher` function for some boilerplate code

--- main.go
+++ main.go
@@ -12,46 +12,34 @@ import (
 
 const port string = ":2323"
 
-func sha1Sum(w http.ResponseWriter, r *http.Request) {
-	textToHash := r.URL.Query().Get("text")
+func hasher(w http.ResponseWriter, hashedText []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	hashedText := sha1.Sum([]byte(textToHash))
-	sha1Hash := hex.EncodeToString(hashedText[:])
-	jsonResq, err := json.Marshal(sha1Hash)
+	hash := hex.EncodeToString(hashedText[:])
+	jsonResq, err := json.Marshal(hash)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	w.Write(jsonResq)
+}
+
+func sha1Sum(w http.ResponseWriter, r *http.Request) {
+	textToHash := r.URL.Query().Get("text")
+	hashedText := sha1.Sum([]byte(textToHash))
+	hasher(w,hashedText[:])
 }
 
 func sha256Sum(w http.ResponseWriter, r *http.Request) {
 	textToHash := r.URL.Query().Get("text")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 	hashedText := sha256.Sum256([]byte(textToHash))
-	sha1Hash := hex.EncodeToString(hashedText[:])
-	jsonResq, err := json.Marshal(sha1Hash)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Write(jsonResq)
+	hasher(w,hashedText[:])
 }
 
 func md5sum(w http.ResponseWriter, r *http.Request) {
 	textToHash := r.URL.Query().Get("text")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
 	hashedText := md5.Sum([]byte(textToHash))
-	md5Hashed := hex.EncodeToString(hashedText[:])
-	jsonResq, err := json.Marshal(md5Hashed)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Write(jsonResq)
+	hasher(w,hashedText[:])
 }
 
 func main() {
